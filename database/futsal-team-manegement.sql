52111ba1ae85db7affa789062775fd9ad7912423
Jiyun Yun <jeeyunyun8@gmail.com>
2024-01-16 16:59:34 +0900

    fix: 테이블 로직 수정

--- database/futsal-team-manegement.sql
+++ database/futsal-team-manegement.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE `User` (
 	`user_id`	INTEGER	NOT NULL	COMMENT 'PK',
-	`email`	VARCHAR(100)	NOT NULL	COMMENT 'unique, Kakao API',
-	`password`	VARCHAR(20)	NOT NULL,
+	`email`	VARCHAR(100)	NULL	COMMENT 'unique, Kakao API',
+	`password`	VARCHAR(20)	NULL,
 	`user_name`	VARCHAR(20)	NOT NULL	COMMENT 'Kakao API',
 	`gender`	VARCHAR(10)	NOT NULL	COMMENT 'Kakao API',
 	`birth_date`	DATE	NOT NULL	COMMENT 'Kakao API',
 	`phone_number`	VARCHAR(15)	NOT NULL	COMMENT 'unique, Kakao API',
-	`nickname`	VARCHAR(50)	NOT NULL	COMMENT 'unique',
+	`nickname`	VARCHAR(50)	NULL	COMMENT 'unique',
+	`is_guest`	BOOL	NOT NULL,
 	`memo`	VARCHAR(200)	NULL
 );
 
@@ -26,8 +27,9 @@ CREATE TABLE `TeamSchedule` (
 	`site`	VARCHAR(100)	NOT NULL,
 	`type`	VARCHAR(50)	NOT NULL,
 	`is_public`	BOOL	NOT NULL	COMMENT 'T: public / F: private',
-	`participation_limit`	BOOL	NOT NULL,
-	`participation_number`	INTEGER	NULL,
+	`is_limit`	BOOL	NOT NULL,
+	`limit`	INTEGER	NULL,
+	`applicant`	INTEGER	NULL,
 	`memo`	VARCHAR(200)	NULL
 );
 
@@ -37,15 +39,6 @@ CREATE TABLE `Comment` (
 	`exercise_id`	INTEGER	NOT NULL	COMMENT 'FK',
 	`team_id`	INTEGER	NOT NULL	COMMENT 'FK',
 	`content`	VARCHAR(200)	NOT NULL
-);
-
-CREATE TABLE `NonUser` (
-	`non_user_id`	INTEGER	NOT NULL	COMMENT 'PK',
-	`non_user_name`	VARCHAR(20)	NOT NULL,
-	`gender`	VARCHAR(10)	NOT NULL,
-	`birth_date`	DATE	NOT NULL,
-	`phone_number`	VARCHAR(15)	NOT NULL	COMMENT 'unique, Kakao API',
-	`memo`	VARCHAR(200)	NULL
 );
 
 CREATE TABLE `Members` (
@@ -58,7 +51,7 @@ CREATE TABLE `Participant` (
 	`exercise_id`	INTEGER	NOT NULL	COMMENT 'FK, PK',
 	`team_id`	INTEGER	NOT NULL	COMMENT 'FK, PK',
 	`user_id`	INTEGER	NOT NULL	COMMENT 'FK',
-	`non_user_id`	INTEGER	NOT NULL	COMMENT 'FK'
+	`approval`	BOOL	NULL
 );
 
 ALTER TABLE `User` ADD CONSTRAINT `PK_USER` PRIMARY KEY (
@@ -77,10 +70,6 @@ ALTER TABLE `TeamSchedule` ADD CONSTRAINT `PK_TEAMSCHEDULE` PRIMARY KEY (
 ALTER TABLE `Comment` ADD CONSTRAINT `PK_COMMENT` PRIMARY KEY (
 	`comment_id`,
 	`user_id`
-);
-
-ALTER TABLE `NonUser` ADD CONSTRAINT `PK_NONUSER` PRIMARY KEY (
-	`non_user_id`
 );
 
 ALTER TABLE `Members` ADD CONSTRAINT `PK_MEMBERS` PRIMARY KEY (
@@ -149,16 +138,10 @@ REFERENCES `TeamSchedule` (
 	`team_id`
 );
 
-ALTER TABLE `Participant` ADD CONSTRAINT `FK_Members_TO_Participant_1` FOREIGN KEY (
+ALTER TABLE `Participant` ADD CONSTRAINT `FK_User_TO_Participant_1` FOREIGN KEY (
 	`user_id`
 )
-REFERENCES `Members` (
+REFERENCES `User` (
 	`user_id`
 );
 
-ALTER TABLE `Participant` ADD CONSTRAINT `FK_NonUser_TO_Participant_1` FOREIGN KEY (
-	`non_user_id`
-)
-REFERENCES `NonUser` (
-	`non_user_id`
-);
